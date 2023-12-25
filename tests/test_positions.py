abbfe8e6c0481c0a5472ac4cfaa4fcd10d5f1a49
Prabhu K <prabhu.k@icloud.com>
2023-12-26 02:53:36 +0530

    Update test_positions.py

--- tests/test_positions.py
+++ tests/test_positions.py
@@ -12,13 +12,25 @@ class TestPositions:
         mock_dhan_client.get_positions.return_value = mock_positions_output
         assert len(mock_positions.get) == 1
 
-    def test_exists(self, mock_dhan_client, mock_positions_output):
+    def test_spot_exists(self, mock_dhan_client, mock_positions_output):
         mock_positions = Positions(dhan_client=mock_dhan_client)
         mock_position_type = mock_positions_output["data"][0]["positionType"]
         mock_dhan_client.get_positions.return_value = mock_positions_output
         assert (
-            mock_positions.exists(
+            mock_positions.spot_exists(
                 symbol_name="MIDCPNIFTY", position_type=mock_position_type
+            )
+            is True
+        )
+
+    def test_strike_exists(self, mock_dhan_client, mock_positions_output):
+        mock_positions = Positions(dhan_client=mock_dhan_client)
+        mock_security_id = mock_positions_output["data"][0]["securityId"]
+        mock_position_type = mock_positions_output["data"][0]["positionType"]
+        mock_dhan_client.get_positions.return_value = mock_positions_output
+        assert (
+            mock_positions.strike_exists(
+                security_id=mock_security_id, position_type=mock_position_type
             )
             is True
         )
